a99bbd5626a3d0a83c990900263e8a741c2b513c
孙通 <17701253801@163.com>
2024-03-05 07:41:48 +0000

    ADD: body to person

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -8,6 +9,11 @@ import (
 func main() {
 	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "OK!")
+		b := r.Body
+		defer b.Close()
+		var person Person
+		json.NewDecoder(b).Decode(&person)
+		fmt.Println(person)
 	})
 	http.ListenAndServe(":8890", nil)
 }
